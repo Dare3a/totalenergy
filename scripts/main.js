372d7a80217e2870e0e82aaa198893253d1c2eab
darko2019 <darko.ilic83@gmail.com>
2025-01-29 14:30:45 +0100

    Postavljen bottom menu

--- scripts/main.js
+++ scripts/main.js
@@ -29,3 +29,22 @@ document.addEventListener('click', (event) => {
 });
 //kraj navigacije
 
+// SCROLL TO TOP
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const button = document.querySelector('#scroll-top');
+
+    button.addEventListener('click', function () {
+        window.scrollTo({top: 0, left: 0, behavior: "smooth"});
+    });
+
+    // window.addEventListener('scroll', function () {
+    //     if (window.scrollY < 200) {
+    //         button.style.opacity = "0";
+    //     } else {
+    //         button.style.opacity = "1";
+    //     }
+    // });
+
+});
+
