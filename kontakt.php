6d685be922093f0033d48d363bc0cb63097473c0
darko2019 <darko.ilic83@gmail.com>
2025-01-30 13:54:23 +0100

    Napravljena PHP kontakt stranica

---
+++ kontakt.php
@@ -0,0 +1,27 @@
+<?php
+if (isset($_POST["submit"])) {
+    $name = $_POST['name'];
+    $company = $_POST['company'];
+    $email = $_POST['email'];
+    $telefon = $_POST['telefon'];
+    $message = $_POST['message'];
+
+
+    $to = 'office@totalenergy.rs';
+    $subject = 'Contact form';
+    $body = "Ime: $name\r\n\nE-mail: $email\n\nTelefon: $telefon\n\nPoruka: $message";
+
+// More headers
+    $headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n";
+    $headers .= 'Reply-To: ' . $name . ' <' . $email . '>' . "\r\n";
+    $headers .= 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-Type: text/plain; charset=UTF-8' . "\r\n";
+    $headers .= 'Content-Transfer-Encoding: 8bit' . "\r\n";
+    if (mail($to, $subject, $body, $headers)) {
+        $result = true;
+    }
+
+}
+include 'index.html';
+?>
+<?php
